105a087ddb0378dce63859c66f7f8d6f7e23d443
Theo Naunheim <theonaunheim@gmail.com>
2019-05-27 21:34:32 -0500

    First working-ish version.

--- scratch.py
+++ scratch.py
@@ -1,27 +1,26 @@
 import pyfair
 
+# Create model and calculate.
 model1 = pyfair.FairModel(name="Regular Model 1", n_simulations=10_000)
 model1.input_data('Loss Event Frequency', low=20, mode=100, high=900)
 model1.input_data('Probable Loss Magnitude', low=3_000_000, mode=3_500_000, high=5_000_000)
 model1.calculate_all()
 
+# Create another model
 model2 = pyfair.FairModel(name="Regular Model 2", n_simulations=10_000)
 model2.input_data('Loss Event Frequency', low=.5, mode=.7, high=.9)
 model2.input_data('Probable Loss Magnitude', low=2_000_000_000, mode=3_000_000_000, high=5_000_000_000)
 model2.calculate_all()
 
-model3 = pyfair.FairModel(name="Regular Model 3", n_simulations=10_000)
-model3.input_data('Loss Event Frequency', low=10, mode=15, high=16)
-model3.input_data('Probable Loss Magnitude', low=500_000_000, mode=500_900_000, high=600_000_000)
-model3.calculate_all()
+# Load a model from a serialized json file
+with open('./serialized_model.json') as f:
+    json = f.read()
+    model3 = pyfair.FairModel.read_json(json)
 
-#fr = FairReport(model1)
-#fr.to_html('C:/Users/theon/Desktop/report.html')
-results = model1.export_results()
-node_statuses = model1.get_node_statuses()
-
-mm = pyfair.FairMetaModel(name='My Meta Model!', models=[model1, model2])
+# Create metamodel
+mm = pyfair.FairMetaModel(name='My Meta Model!', models=[model1, model3])
 mm.calculate_all()
 
+# Create and output report.
 fsr = pyfair.FairSimpleReport([model3, mm])
 fsr.to_html('output.html')
